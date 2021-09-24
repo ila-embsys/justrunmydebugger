42cd269cf13cac8ec040d9f5037cc3dbae192955
D. Lisin <mrlisdim@gmail.com>
2021-09-24 03:16:05 +0300

    [rust] Make `Kind` enum serialization to ints

--- src-tauri/src/openocd/event.rs
+++ src-tauri/src/openocd/event.rs
@@ -2,10 +2,11 @@ use tauri::Window;
 
 use crate::api;
 
-#[derive(Clone, serde::Serialize)]
+#[derive(Clone, serde_repr::Serialize_repr, serde_repr::Deserialize_repr)]
+#[repr(u8)]
 pub enum Kind {
-    Start,
-    Stop,
+    Start = 0,
+    Stop = 1,
 }
 
 #[derive(Clone, serde::Serialize)]
